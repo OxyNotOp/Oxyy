36561083852632620e702a5041a38f75b94fa7c7
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-12-03 23:54:19 +0530

    Add files via upload

--- Music/Plugins/sudo.py
+++ Music/Plugins/sudo.py
@@ -25,7 +25,7 @@ async def useradd(_, message: Message):
             return await message.reply_text("✅ Aleady a Sudo User.")
         added = await add_sudo(user.id)
         if added:
-            await message.reply_text(f"✅ Added **{user.mention}** as a Super User for Oxy OwO")
+            await message.reply_text(f"✅ Added **{user.mention}** as a Super User for Yukki OwO")
             return os.execvp("python3", ["python3", "-m", "Music"])
         await edit_or_reply(message, text="❌ Something wrong happened, check logs.")  
         return
@@ -37,7 +37,7 @@ async def useradd(_, message: Message):
         return await message.reply_text("✅ Already a Sudo User.")
     added = await add_sudo(user_id)
     if added:
-        await message.reply_text(f"✅ Added **{mention}** as a Super User for Oxy OwO")
+        await message.reply_text(f"✅ Added **{mention}** as a Super User for Yukki OwO")
         return os.execvp("python3", ["python3", "-m", "Music"])
     await edit_or_reply(message, text="❌ Something wrong happened, check logs.")  
     return    
@@ -55,10 +55,10 @@ async def userdel(_, message: Message):
         user = (await app.get_users(user))
         from_user = message.from_user      
         if user.id not in await get_sudoers():
-            return await message.reply_text(f"❌ Not a part of Oxy's Sudo.")        
+            return await message.reply_text(f"❌ Not a part of Yukki's Sudo.")        
         removed = await remove_sudo(user.id)
         if removed:
-            await message.reply_text(f"✅ Removed **{user.mention}** from Oxy's Sudo.")
+            await message.reply_text(f"✅ Removed **{user.mention}** from Yukki's Sudo.")
             return os.execvp("python3", ["python3", "-m", "Music"])
         await message.reply_text(f"❌ Something wrong happened.")
         return
@@ -66,10 +66,10 @@ async def userdel(_, message: Message):
     user_id = message.reply_to_message.from_user.id
     mention = message.reply_to_message.from_user.mention
     if user_id not in await get_sudoers():
-        return await message.reply_text(f"❌ Not a part of Oxy's Sudo.")        
+        return await message.reply_text(f"❌ Not a part of Yukki's Sudo.")        
     removed = await remove_sudo(user_id)
     if removed:
-        await message.reply_text(f"✅ Removed **{mention}** from Oxy's Sudo.")
+        await message.reply_text(f"✅ Removed **{mention}** from Yukki's Sudo.")
         return os.execvp("python3", ["python3", "-m", "Music"])
     await message.reply_text(f"❌ Something wrong happened.")
                 
