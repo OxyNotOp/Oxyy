c4066411982393c7b3b5a5affe45824c0ad6eaf3
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-12-26 17:48:03 +0530

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -15,6 +15,6 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://music:music@cluster0.sh6h4.
 SUDO_USERS = list(map(int, getenv('SUDO_USERS', '1303669398').split()))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ''))
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "FEELINGSOP")
-OXY_SUPPORT = getenv("ZAID_SUPPORT", "bestiesfortheresties")
+OXY_SUPPORT = getenv("OXY_SUPPORT", "bestiesfortheresties")
 ASS_ID = int(getenv("ASS_ID", ''))
 OWNER_ID = list(map(int, getenv('OWNER_ID', '').split()))
